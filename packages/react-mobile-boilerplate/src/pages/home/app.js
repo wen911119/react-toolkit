4e967bbcd5ac53c706a8bc082d60250d9c917b60
wen911119 <wen911119@gmail.com>
2018-08-13 15:27:04 +0800

    commit-test

---
+++ packages/react-mobile-boilerplate/src/pages/home/app.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+export default class HomePage extends Component {
+  componentDidMount() {
+    console.log(13331)
+  }
+  render () {
+    return <div>hello</div>
+  }
+}
