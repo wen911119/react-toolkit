b50c84a50a92600527677c2d129dca76c09f300d
wen911119 <wen911119@gmail.com>
2018-08-26 23:37:01 +0800

    完善例子

--- packages/react-mobile-boilerplate/src/pages/tabsAndScrollerDemo/app.js
+++ packages/react-mobile-boilerplate/src/pages/tabsAndScrollerDemo/app.js
@@ -7,8 +7,10 @@ import { ScrollerWithRefreshAndLoadMore } from 'react-m-scroller'
 export default class HomePage extends Component {
   constructor (props) {
     super(props)
+    this.onRefresh = this.onRefresh.bind(this)
+    this.onLoadMore = this.onLoadMore.bind(this)
     this.state = {
-      list: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+      list: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     }
   }
   componentDidMount () {}
@@ -17,11 +19,30 @@ export default class HomePage extends Component {
     console.log(index)
   }
 
+  onRefresh (done) {
+    setTimeout(() => {
+      this.setState({ list: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] }, done)
+    }, 1500)
+  }
+  onLoadMore (done) {
+    setTimeout(() => {
+      let _list = Array.from(this.state.list)
+      for (let l = _list.length, i = l + 1; i < l + 11; i++) {
+        _list.push(i)
+      }
+      this.setState({ list: _list }, _ => done(_list.length >= 60))
+    }, 1000)
+  }
+
   render () {
     return (
       <div style={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
         <Tabs onChange={this.onTabsChange} titles={['tab1', 'tab2']} fill>
-          <ScrollerWithRefreshAndLoadMore height='100%'>
+          <ScrollerWithRefreshAndLoadMore
+            height='100%'
+            onRefresh={this.onRefresh}
+            onLoadMore={this.onLoadMore}
+          >
             <div>
               {this.state.list.map(item => (
                 <XCenterView height={200} key={item}>
