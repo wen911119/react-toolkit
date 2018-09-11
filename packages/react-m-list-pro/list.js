20bb078e69c662de19cbfd58a8f30828021c6d1d
wen911119 <wen911119@gmail.com>
2018-09-11 15:14:21 +0800

    update:list-pro增加keyExtractor

--- packages/react-m-list-pro/list.js
+++ packages/react-m-list-pro/list.js
@@ -5,7 +5,11 @@ export default class ListPro extends PureComponent {
     return (
       <div>
         {this.props.data.map((item, index) => (
-          <div key={index}>
+          <div
+            key={
+              this.props.keyExtractor ? this.props.keyExtractor(item) : index
+            }
+          >
             {this.props.renderItem(item, index, this.props.extraData)}
           </div>
         ))}
